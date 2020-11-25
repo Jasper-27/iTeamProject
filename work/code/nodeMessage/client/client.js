e3e4d7fdd2684301164dceb036b7fdb4fa6a0a4a
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2020-11-25 12:27:22 +0000

    added basic node messaging code
    This code is just an example of how sockets work in nodeJS

---
+++ work/code/nodeMessage/client/client.js
@@ -0,0 +1,19 @@
+const { read } = require("fs");
+const { send } = require("process");
+const io = require("socket.io-client");
+
+
+// The server address 
+let socket = io.connect("http://localhost:1234"); 
+
+socket.on("welcome", (data) => {
+    console.log("Received: ", data); 
+    socket.emit("check", "Check")
+}); 
+
+
+
+function sendMessage(){
+    console.log("Me: " + "msg")
+    socket.emit("send-message", "msg")
+}; 
---
+++ work/code/nodeMessage/server/server.js
@@ -0,0 +1,33 @@
+const express = require("express"); 
+//const { Socket } = require("socket.io-client");
+const app = express();
+const port = 1234;
+const http = require("http").createServer(); 
+
+const io = require("socket.io")(http); 
+
+io.on("connection", (socket) => {
+
+    // When connect, send message 
+    //socket.emit("welcome", "Welcome to my server"); 
+    console.log("Client connected"); 
+
+
+    socket.on('disconnect', function (){
+        console.log("Client Disconnected")
+    }); 
+
+    socket.on("send-message", (data) => {
+        console.log("message: ", data); 
+    }); 
+
+
+
+
+
+    
+}); 
+
+http.listen(port, () => {
+    console.log("Server is listing on localhost: " + port); 
+}); 
