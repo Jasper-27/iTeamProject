2ce5af7bcf482d60bdaf4e9f7be2fed975dc79cd
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2021-01-19 19:20:37 +0000

    Changed the hardcoded server to localhost

--- App/Client/script.js
+++ App/Client/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://20.55.102.126:3000')
+const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
