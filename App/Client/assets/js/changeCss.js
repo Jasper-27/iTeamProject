65c4145475ee2dc234f433c69f22fa919c6562d7
Tom Nash <57364342+Tom451@users.noreply.github.com>
2021-02-22 17:00:00 +0000

    Linked all the changes together
    This includes:
    CSS switcher
    New Page layout for login
    CSS organisation overall

--- App/Client/assets/js/changeCss.js
+++ App/Client/assets/js/changeCss.js
@@ -1,12 +1,14 @@
-function changeCSS(cssFile) {
+function changeCSS() {
 
-	window.alert("Working.")
+    var oldlink = document.getElementsByTagName("link").item(1);
+	var sheet = document.getElementById('cssSheet').value
 	
-    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+	var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}'); 
+
 
     var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("href", "assets/css/colour/" + cssFile + "/styles-Colour.css");
+    newlink.setAttribute("href", "assets/css/colour/" + sheet + "/styles-Colour.css");
 
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
