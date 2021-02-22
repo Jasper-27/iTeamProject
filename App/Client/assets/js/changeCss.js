23ac7609db31c9fa08740aadf7466c0d691643f3
Tom Nash <57364342+Tom451@users.noreply.github.com>
2021-02-22 14:47:22 +0000

    Moved the CSS files around

--- App/Client/assets/js/changeCss.js
+++ App/Client/assets/js/changeCss.js
@@ -1,10 +1,12 @@
-function changeCSS(cssFile, cssLinkIndex) {
+function changeCSS(cssFile) {
 
+	window.alert("Working.")
+	
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
 
     var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("href", "assets/css/" + cssFile);
+    newlink.setAttribute("href", "assets/css/colour/" + cssFile + "/styles-Colour.css");
 
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
