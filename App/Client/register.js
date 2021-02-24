7cfe3bb47e50ad1b297e6fbbdb5bd836b20e60dd
Tom Nash <57364342+Tom451@users.noreply.github.com>
2021-02-24 13:49:10 +0000

    Started Work on Username Constraints
    - Added username Size check
    - Stopped the user adding a blank name
    - Revamped the registration page (even if it is only temporary)
    - Added console logs to see where the user currently is within the program

--- App/Client/register.js
+++ App/Client/register.js
@@ -1,4 +1,5 @@
 const socket = io('http://localhost:3000'); 
+
         
 
 // Tell the user when a registration has failed 
@@ -11,17 +12,45 @@ socket.on('register-success', () => {alert('Account created')});
 
 // Checks to see if details are valid, then sends them on to the server. 
 function register(){
-    
-    if (password1.value != password2.value){
-        alert("Passwords don't match");
+	
+	var passwordValue = document.getElementById("password1Entry").value 
+	var password2Value = document.getElementById("password2Entry").value 
+	var usernameinput = document.getElementById("usernameEntry").value 
+
+	
+	//blank check 
+	if (usernameinput == null){
+		alert("UserName Empty");
+		console.log("username1");
         return; 
-    }else if (password1.value.trim() != password1.value){
+	}
+	else if(usernameinput.length > 20 ){
+		alert("Username Too Long");
+		console.log("username2");
+        return; 
+	}
+	
+	
+	//check if passwords are the same, if not error 
+    if (passwordValue != password2Value){
+        alert("Passwords not the same");
+		console.log("password1");
+        return; 
+		
+	// ensure that the password does not have a blank space
+    }else if (passwordValue.trim() != passwordValue){
         alert("Passwords can't have blank space");
+		console.log("password2");
         return;
-    }else if (password1.value.length < 1 || password2.value.length < 1 || username.value.length < 1){ 
+		
+	//if the HTML doesnt catch blank entry 
+    }else if (passwordValue.length < 1 || password2Value.length < 1 || usernameinput.length < 1){ 
         alert("You have missing values");
+		console.log("password3");
         return;
     }else{
-        socket.emit('create-account', {"username": username.value, "firstName": firstName.value, "lastName": lastName.value, "password": password1.value});
+		alert("User Created")
+        socket.emit('create-account', {"username": usernameinput, "firstName": firstName.value, "lastName": lastName.value, "password": passwordValue});
+		console.log("done");
     }
 }
