8cbf94ce060b6795f07d594c8eec797e68c0a994
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2021-03-08 15:42:05 +0000

    Fixed some things on the registration page

--- App/Client/register.js
+++ App/Client/register.js
@@ -22,36 +22,48 @@ function register(){
 	//blank check 
 	if (usernameinput == null){
 		alert("UserName Empty");
-		console.log("username1");
         return; 
-	}
-	else if(usernameinput.length > 20 ){
+	}else if(usernameinput.length > 20 ){
 		alert("Username Too Long");
-		console.log("username2");
         return; 
-	}
-	
-	
-	//check if passwords are the same, if not error 
-    if (passwordValue != password2Value){
+    }else if (passwordValue != password2Value){  //check if passwords are the same, if not error 
         alert("Passwords not the same");
-		console.log("password1");
         return; 
-		
-	// ensure that the password does not have a blank space
-    }else if (passwordValue.trim() != passwordValue){
+    }else if (passwordValue.trim() != passwordValue){  // ensure that the password does not have a blank space
         alert("Passwords can't have blank space");
-		console.log("password2");
         return;
-		
-	//if the HTML doesnt catch blank entry 
-    }else if (passwordValue.length < 1 || password2Value.length < 1 || usernameinput.length < 1){ 
+    }else if (passwordValue.length < 1 || password2Value.length < 1 || usernameinput.length < 1){  //if the HTML doesnt catch blank entry 
         alert("You have missing values");
-		console.log("password3");
         return;
     }else{
-		alert("User Created")
         socket.emit('create-account', {"username": usernameinput, "firstName": firstName.value, "lastName": lastName.value, "password": passwordValue});
-		console.log("done");
     }
+
+
+    // Checks to see if details are valid, then sends them on to the server. 
+
 }
+
+// function register(){
+
+//     var password1 = document.getElementById("password1Entry").value 
+//     var password2 = document.getElementById("password2Entry").value 
+//     var username = document.getElementById("usernameEntry").value 
+    
+//     if (password1 != password2){
+//         alert("Passwords don't match");
+//         return; 
+//     }else if (username.length > 20){
+//         alert("username is too long")
+//         return; 
+//     }
+//     else if (password1.trim() != password1){
+//         alert("Passwords can't have blank space");
+//         return;
+//     }else if (password1.length < 1 || password2.length < 1 || username.length < 1){ 
+//         alert("You have missing values");
+//         return;
+//     }else{
+//         socket.emit('create-account', {"username": username.value, "firstName": firstName.value, "lastName": lastName.value, "password": password1.value});
+//     }
+// }
