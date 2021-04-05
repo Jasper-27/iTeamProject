1cf65489425cd67c00fc1a3d44133e5c67284115
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2021-04-05 15:21:55 +0100

    Revert "Moved the "X is typing" out of my authentication code bit"
    This reverts commit 5a8d9b828c88a18762a6eaa05ca72dbc5fac9ea2.

--- App/Client/script.js
+++ App/Client/script.js
@@ -471,18 +471,6 @@ function attemptAuth(){
 }
 
 
-function renewAuth(){
-  console.log("renewAuth")
-  socket.emit('renew-auth', {"token": sessionStorage.token, "username" : sessionStorage.username})
-}
-
-
-// Checking in with the server every X amount of times 
-const heartBeatReauth = setInterval(function() { renewAuth() }, 20000)
-
-// =============================================================================
-
-
 
 // Listen for when client starts typing
 messageInput.addEventListener('keypress', inUsername => { 
@@ -520,3 +508,14 @@ function invisible(){
 function timer(){
   typingTimer = false;
 }
+
+function renewAuth(){
+  console.log("renewAuth")
+  socket.emit('renew-auth', {"token": sessionStorage.token, "username" : sessionStorage.username})
+}
+
+
+// Checking in with the server every X amount of times 
+const heartBeatReauth = setInterval(function() { renewAuth() }, 20000)
+
+// =============================================================================
